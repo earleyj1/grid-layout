969ded5a686c3002140b50093fbf509feff21153
Jesse Earley <earleyj1@msu.edu>
2018-07-18 06:56:06 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            dist: {
+                options: {
+                style: 'compressed',
+            
+                sourcemap: 'none'
+                },
+                files: {
+                'dist/grid-framework/grid-layout.min.css': [
+                    'css/grid-layout.scss']
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'assets/build/app.min.js': ['assets/js/**/*.js']
+                },
+            }
+        },
+        watch: {
+        options: {
+            livereload: true
+        },
+        sass: {
+            files: ['sass/**/*.scss'],
+            tasks: ['sass']
+        },
+        js: {
+            files: ['assets/js/**/*.js'],
+            tasks: ['jshint', 'uglify']
+        },
+        html: {
+            files: ['*.html']
+        }
+        },
+        clean: {
+            dist: ['assets/build/app.min.css','assets/build/app.min.js']
+        }
+    });
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    // Register tasks
+    grunt.registerTask('default', ['clean','sass','uglify']);
+};
